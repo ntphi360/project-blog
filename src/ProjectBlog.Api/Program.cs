1a4a2c1c6e1455cef39b96cd0a8624c7ee7f0aff
ntphi360 <ntphi360@gmail.com>
2026-01-04 22:08:34 +0700

    Create context and service db program

--- src/ProjectBlog.Api/Program.cs
+++ src/ProjectBlog.Api/Program.cs
@@ -1,7 +1,41 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProjectBlog.Core.Domain.identity;
+using ProjectBlog.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
+// Config DB Context and ASP.NET Core Identity
+// Add services to the container
+builder.Services.AddDbContext<ProjectBlogContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<AppUser,AppRole>(options => options.SignIn.RequireConfirmedAccount = false);
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+
+    // Lockout settings.
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = false;
+
+    // User settings.
+    options.User.AllowedUserNameCharacters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+});
+
+//Default config for ASP.NET Core
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
---
+++ src/ProjectBlog.Core/Domain/identity/AppRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ProjectBlog.Core.Domain.identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName { get; set; }
+    }
+}
